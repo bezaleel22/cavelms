42a05287f9283973aed9a4cda4a9f9a54a9e0c7a
Onojeta Brown <onosbrown.saved@gmail.com>
2023-04-02 08:50:55 +0100

    update

---
+++ apps/webapp/houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "http://localhost:8000/api/query"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
