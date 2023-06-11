c325887570b45445912b52040a42c13ad2a4fb9c
Noah Abayomi <binary.rln@gmail.com>
2023-06-11 18:07:01 +0100

    fixed login page

--- apps/webapp/src/app.d.ts
+++ apps/webapp/src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { AuthUser } from "$lib/store/authentication";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: AuthUser
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
