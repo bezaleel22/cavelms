cc5556ed55989a0dafbd28fbcfd150838bacb6e9
Onojeta Brown <onosbrown.saved@gmail.com>
2023-08-24 06:21:38 +0100

    update

--- apps/webapp/src/lib/components/brand.svelte
+++ apps/webapp/src/lib/components/brand.svelte
@@ -1,8 +1,11 @@
-<div class="z-20 sticky top-0 items-center gap-2 px-4 py-2 hidden lg:flex ">
+<script lang="ts">
+  import logo from "$lib/assets/images/logo11.svg";
+</script>
+
+<div class="z-20 sticky top-0 items-center gap-2 px-4 py-2 hidden lg:flex mb-10">
   <a href="/" class="flex-0 btn btn-ghost px-2">
-    <div class="font-title text-primary inline-flex text-lg transition-all duration-200 md:text-3xl mx-auto sm:text-2xl">
-      <span class="capitalize">Adu</span>
-      <span class="text-base-content uppercase">LLAM</span>
+    <div class="w-52 h-12 rounded">
+      <img src={logo} alt="" />
     </div>
   </a>
 </div>
