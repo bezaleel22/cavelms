9182c9307e71d4fe8714069324e7631e549aa7aa
Onojeta Brown <onosbrown.saved@gmail.com>
2024-05-29 13:01:46 +0100

    Update +page.svelte

--- website/src/routes/(academics)/pgdt/+page.svelte
+++ website/src/routes/(academics)/pgdt/+page.svelte
@@ -28,7 +28,7 @@
             <li>
               <div class="content-box">
                 <h5 class="description">
-                  <strong>WHAT YOU WILL LEARN and Many More...</strong><br />
+                  <strong>WHAT YOU WILL LEARN and Many More...!!!!</strong><br />
                   - Develop foundational understanding of Biblical Truth<br />
                   - Develop contemporary strategies for effective Ministry within various contexts<br
                   />
